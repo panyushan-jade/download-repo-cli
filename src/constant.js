ae7b9f07ce50fe8f2d4fc03ea6439c504fbb12cf
panyushan <1193345664@qq.com>
2023-06-17 11:59:27 +0800

    fixbug：修复pkg读取路径问题

--- src/constant.js
+++ src/constant.js
@@ -1,10 +1,13 @@
 
 import os from 'os';
 import path from 'path';
-import fs from 'fs-extra'
+import fs from 'fs-extra';
+import { fileURLToPath } from 'url'
+
 
 const TEMPFILEPATH = path.join(os.tmpdir(),'dl-repo-cli-cache.json');
-const PKG = fs.readJsonSync('package.json');
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+const PKG = fs.readJsonSync(path.resolve(__dirname,'../package.json'));
 const GITHUB = 'github';
 const GITEE = 'gitee';
 
